3601e289728f56158138fb0d7ae01b3b94bdf90c
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-02 17:24:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
+git clone https://github.com/Lazyindu/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
 cd /LazyUrlHunterrBOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... LazyDeveloperrr"
+echo "Starting LazyUrlHunterrBOT....."
 python3 main.py
